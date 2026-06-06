017ffce4e9cb6b32799422c637d6e76fa6acdc03
ThePrestigeTreeGuy <115898719+ThePrestigeTreeGuy@users.noreply.github.com>
2026-06-06 12:43:59 -0400

    e

--- js/mod.js
+++ js/mod.js
@@ -18,9 +18,8 @@ let VERSION = {
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.1</h3><br>
-		- Ad<br>
-		- Ad<br>
-		- End`
+		- Added 1 layer (succession)<br>
+		- Endgame: 10`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
@@ -74,7 +73,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.S.points.gte(new Decimal("e15"))
+	return player.S.number.gte(new Decimal("e15"))
 }
 
 
