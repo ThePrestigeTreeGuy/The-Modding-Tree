dd2264b8debbf9d92d9cd2af20e33239e30976cc
ThePrestigeTreeGuy <115898719+ThePrestigeTreeGuy@users.noreply.github.com>
2026-06-25 13:15:32 -0400

    aa

--- js/mod.js
+++ js/mod.js
@@ -33,7 +33,7 @@ let changelog = `<h1>Changelog:</h1><br>
 		- Added 1 layer (succession)<br>
 		- Endgame: 10`
 
-let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+let winText = `Congratulations! You have reached the end and beaten this game, for now...`
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
